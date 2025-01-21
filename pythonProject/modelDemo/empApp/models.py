6ceb497c2d5d15bec529a90243125dfd68a379d4
rahulsubudhi <a.rahulsubudhi2022@gmail.com>
2025-01-21 13:28:35 +0530

    learned relationship

--- pythonProject/modelDemo/empApp/models.py
+++ pythonProject/modelDemo/empApp/models.py
@@ -7,3 +7,30 @@ class Employee(models.Model):
     lastname=models.CharField(max_length=30)
     salary=models.FloatField()
     email=models.CharField(max_length=35)
+
+class Programer(models.Model):
+    name=models.CharField(max_length=30)
+    sal=models.IntegerField()
+
+class Project(models.Model):
+    name=models.CharField(max_length=30)
+    programers=models.ManyToManyField(Programer)
+
+class Customer(models.Model):
+    name=models.CharField(max_length=30)
+
+class PhoneNumber(models.Model):
+    type=models.CharField(max_length=10)
+    number=models.CharField(max_length=10)
+    customer=models.ForeignKey(Customer,on_delete=models.CASCADE)
+
+class Person(models.Model):
+    firstName=models.CharField(max_length=30)
+    lastname=models.CharField(max_length=30)
+    age=models.IntegerField()
+
+class License(models.Model):
+    type=models.CharField(max_length=30)
+    validFrom=models.DateField()
+    validTo=models.DateField()
+    person=models.OneToOneField(Person,on_delete=models.CASCADE)
